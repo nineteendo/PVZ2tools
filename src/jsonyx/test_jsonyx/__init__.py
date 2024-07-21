a315a05891585291274e69cfd1e320fc56681d48
Nineteendo <nineteendo19d0@gmail.com>
2024-07-21 21:49:29 +0200

    Test DuplicateKey

--- src/jsonyx/test_jsonyx/__init__.py
+++ src/jsonyx/test_jsonyx/__init__.py
@@ -1,8 +1,11 @@
 # Copyright (C) 2024 Nice Zombies
+# TODO(Nice Zombies): test jsonyx.JSONDecoder
+# TODO(Nice Zombies): test jsonyx.JSONEncoder
+# TODO(Nice Zombies): test jsonyx.JSONSyntaxError
+# TODO(Nice Zombies): test jsonyx.dump
 # TODO(Nice Zombies): test jsonyx.dumps
 # TODO(Nice Zombies): test jsonyx.format_syntax_error
-# TODO(Nice Zombies): test jsonyx.DuplicateKey
-# TODO(Nice Zombies): test jsonyx.JSONSyntaxError
+# TODO(Nice Zombies): test jsonyx.load
 """JSON tests."""
 from __future__ import annotations
 
@@ -36,3 +39,10 @@ def get_json(request: pytest.FixtureRequest) -> ModuleType:
         pytest.skip("requires jsonyx._speedups")
 
     return json
+
+
+def test_duplicate_key(json: ModuleType) -> None:
+    """Test DuplicateKey."""
+    string: str = json.DuplicateKey("a")
+    assert str(string) == "a"
+    assert hash(string) == id(string)
