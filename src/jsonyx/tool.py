783255b74723aaab077786c411bc1617162cedc9
Nice Zombies <nineteendo19d0@gmail.com>
2024-08-04 13:15:10 +0200

    Bump jsonyx from 1.1.0 to 1.2.1

--- src/jsonyx/tool.py
+++ src/jsonyx/tool.py
@@ -5,12 +5,11 @@ from __future__ import annotations
 __all__: list[str] = ["JSONNamespace", "register", "run"]
 
 import sys
-from pathlib import Path
 from sys import stderr, stdin
 from typing import TYPE_CHECKING
 
-from jsonyx import JSONSyntaxError, dump, format_syntax_error, loads
-from jsonyx.allow import EVERYTHING, NOTHING, SURROGATES
+from jsonyx import Decoder, Encoder, JSONSyntaxError, format_syntax_error
+from jsonyx.allow import EVERYTHING, NOTHING
 
 if TYPE_CHECKING:
     from argparse import ArgumentParser
@@ -23,9 +22,10 @@ class JSONNamespace:
     compact: bool
     ensure_ascii: bool
     indent: int | str | None
-    filename: str | None
+    input_filename: str | None
     no_commas: bool
     nonstrict: bool
+    output_filename: str | None
     sort_keys: bool
     trailing_comma: bool
     use_decimal: bool
@@ -38,49 +38,73 @@ def register(parser: ArgumentParser) -> None:
     :type parser: ArgumentParser
     """
     parser.add_argument(
-        "filename",
-        nargs="?",
-        help="the JSON file to be validated or pretty-printed",
-    )
-    parser.add_argument(
-        "--compact",
-        action="store_true",
-        help='don\'t add unnecessary whitespace after "," and ":"',
-    )
-    parser.add_argument(
+        "-a",
         "--ensure-ascii",
         action="store_true",
         help="escape non-ascii characters",
     )
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "--indent", type=int, metavar="SPACES", help="indent using spaces",
+    parser.add_argument(
+        "-c",
+        "--compact",
+        action="store_true",
+        help='don\'t add unnecessary whitespace after "," and ":"',
     )
-    group.add_argument(
-        "--indent-tab", action="store_const", const="\t", dest="indent",
-        help="indent using tabs",
-    )
-    group2 = parser.add_mutually_exclusive_group()
-    group2.add_argument(
+    comma_group = parser.add_mutually_exclusive_group()
+    comma_group.add_argument(
+        "-C",
         "--no-commas",
         action="store_true",
-        help="separate items by whitespace",
+        help="separate items by whitespace instead of comma's",
     )
-    group2.add_argument(
+    parser.add_argument(
+        "-d",
+        "--use-decimal",
+        action="store_true",
+        help="use decimal instead of float",
+    )
+    indent_group = parser.add_mutually_exclusive_group()
+    indent_group.add_argument(
+        "-i",
+        "--indent",
+        type=int,
+        metavar="SPACES",
+        help="indent using spaces",
+    )
+    parser.add_argument(
+        "-s",
+        "--sort-keys",
+        action="store_true",
+        help="sort the keys of objects",
+    )
+    parser.add_argument(
+        "-S",
+        "--nonstrict",
+        action="store_true",
+        help="allow all JSON deviations",
+    )
+    comma_group.add_argument(
+        "-t",
         "--trailing-comma",
         action="store_true",
         help="add a trailing comma if indented",
     )
-    parser.add_argument(
-        "--nonstrict", action="store_true", help="allow all JSON deviations",
+    indent_group.add_argument(
+        "-T",
+        "--indent-tab",
+        action="store_const",
+        const="\t",
+        dest="indent",
+        help="indent using tabs",
     )
     parser.add_argument(
-        "--sort-keys", action="store_true", help="sort the keys of objects",
+        "input_filename",
+        nargs="?",
+        help='the path to the input JSON file, or "-" for standard input',
     )
     parser.add_argument(
-        "--use-decimal",
-        action="store_true",
-        help="use decimal instead of float",
+        "output_filename",
+        nargs="?",
+        help="the path to the output JSON file",
     )
 
 
@@ -90,29 +114,12 @@ def run(args: JSONNamespace) -> None:
     :param args: the commandline arguments
     :type args: JSONNamespace
     """
-    if args.filename:
-        filename: str = args.filename
-        s: bytes | str = Path(filename).read_bytes()
-    else:
-        filename = "<stdin>"
-        s = "\n".join(
-            iter(input, ""),
-        ) if stdin.isatty() else stdin.buffer.read()
-
-    try:
-        obj: object = loads(
-            s,
-            allow=EVERYTHING - SURROGATES if args.nonstrict else NOTHING,
-            filename=filename,
-            use_decimal=args.use_decimal,
-        )
-    except JSONSyntaxError as exc:
-        stderr.write("".join(format_syntax_error(exc)))
-        sys.exit(1)
-
-    dump(
-        obj,
-        allow=EVERYTHING - SURROGATES if args.nonstrict else NOTHING,
+    decoder: Decoder = Decoder(
+        allow=EVERYTHING if args.nonstrict else NOTHING,
+        use_decimal=args.use_decimal,
+    )
+    encoder: Encoder = Encoder(
+        allow=EVERYTHING if args.nonstrict else NOTHING,
         ensure_ascii=args.ensure_ascii,
         indent=args.indent,
         item_separator=" " if args.no_commas else (
@@ -122,3 +129,18 @@ def run(args: JSONNamespace) -> None:
         sort_keys=args.sort_keys,
         trailing_comma=args.trailing_comma,
     )
+    try:
+        if args.input_filename and args.input_filename != "-":
+            obj: object = decoder.read(args.input_filename)
+        elif stdin.isatty():
+            obj = decoder.loads("\n".join(iter(input, "")), filename="<stdin>")
+        else:
+            obj = decoder.load(stdin)
+    except JSONSyntaxError as exc:
+        stderr.write("".join(format_syntax_error(exc)))
+        sys.exit(1)
+
+    if args.output_filename:
+        encoder.write(obj, args.output_filename)
+    else:
+        encoder.dump(obj)
